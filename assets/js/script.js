534a058b5209736fa358203b363a108c51045ff7
Evan <evanpauly@hotmail.com>
2022-02-08 19:48:05 -0600

    api keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+var apiKeyGoogle = "AIzaSyA_MCwdAKgVfrMu2ysTeBzeeRi4OkuAo9c"
+var apiKeyGoodreads = "1680e01c37msh7d95652a9fba73dp15927cjsnf2dbb215392e"
+
 document.getElementById("search-books").addEventListener("click", function() {
     alert("List Available Books");
 })
+
+fetch("https://goodreadsraygorodskijv1.p.rapidapi.com/addBookToShelf", {
+	"method": "POST",
+	"headers": {
+		"content-type": "application/x-www-form-urlencoded",
+		"x-rapidapi-host": "GoodreadsraygorodskijV1.p.rapidapi.com",
+		"x-rapidapi-key": "1680e01c37msh7d95652a9fba73dp15927cjsnf2dbb215392e"
+	},
+	"body": {
+		"shelfName": "<REQUIRED>",
+		"apiSecret": "<REQUIRED>",
+		"accessTokenSecret": "<REQUIRED>",
+		"apiKey": "<REQUIRED>",
+		"accessToken": "<REQUIRED>",
+		"bookId": "<REQUIRED>"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
