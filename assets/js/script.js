cd90724d42c6ab6c844b76c57068633d35e5dc4d
Danielyaj4 <95711107+Danielyaj4@users.noreply.github.com>
2022-02-10 16:05:23 -0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,37 @@
-var apiKeyGoogle = "AIzaSyA_MCwdAKgVfrMu2ysTeBzeeRi4OkuAo9c"
-var apiKeyGoodreads = "1680e01c37msh7d95652a9fba73dp15927cjsnf2dbb215392e"
+// vars
+var bookTitle = $("#book-search")
+var bookAuth = $("#book-author")
+var bookDescription = $("#book-description")
+var bookPoster = $("#book-image")
+
+
+
+// api calls and endpoints
+const apiUrlFirst = "https://www.googleapis.com/books/v1/volumes?q="
+
+
+const apiKey = "&key=AIzaSyCkVrQHfJ5Npv5EYn671if30ePZ_8IC1g0"
+
+
+
+
+// fetch function
+function fetchBookInfo() {
+    console.log(bookTitle)
+    fetch(apiUrlFirst + bookTitle)
+    .then(function (res) {
+        if(res.ok){
+            res.json().then(function(data){
+                console.log(data)
+                renderPoster(data)
+                
+
+            })
+        }
+    })
+}
+
+
+
+// event listener on cick
+document.getElementById("search-btn").addEventListener("click", fetchBookInfo())
