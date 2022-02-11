02298404003069bcf94b0d5d4c1ec0a1d7f083cd
Jeremiah <jclanc7507@gmail.com>
2022-02-10 19:32:56 -0500

    edited js, css, and html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,68 +1,60 @@
-var apiKeyGoogle = "AIzaSyA_MCwdAKgVfrMu2ysTeBzeeRi4OkuAo9c"
-var apiKeyGoodreads = "1680e01c37msh7d95652a9fba73dp15927cjsnf2dbb215392e"
+// vars
+var bookTitle = $("#book-search")
+var bookAuth = $("#book-author")
+var bookDescription = $("#book-description")
+var bookPoster = $("#book-image")
 
-//https://www.youtube.com/watch?v=uaMMkaa-P_A
-// slide to top jQuery function/animation?
+// api calls and endpoints
+var apiUrlFirst = "https://www.googleapis.com/books/v1/volumes?q="
+var apiKey = "&key=AIzaSyCkVrQHfJ5Npv5EYn671if30ePZ_8IC1g0"
 
-$(document).ready(function() {
-    var item, title, author, publisher, bookLink, booksImg
-    var outputList = document.getElementById("list-output");
-    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-    var placeHldr = "<img src='https://via.placeholder.com/150'>";
-    var searchData;
-   
-    // listener for search button
-    $("#search").click(function() {
-        //debugger;
-        preventDefault();
-        outputList.innerHTML = ""; // empty html output
-        document.body.style.backgroundImage = "url('')";
-            searchData = $("#search-bar").val();
-            // handling empty search input field
-            if(searchData === "" || searchData === null) {
-                displayError();
-            } else {
-                // console.log(searchData);
-                // $.get("https://www.googleapis.com/books/v1/volumes?q=" + searchData, getBookData());
-                $.ajax({
-                    url: bookUrl + searchData,
-                    dataType: "json",
-                    success: function(response) {
-                        console.log(response)
-                        if(response.totalItem === 0) {
-                            alert("No results available. Try a different search.");
-                        } else {
-                            $("title").anitem({'margin-top': '5px'}, 1000); //search box animation
-                            $(".book-list").css('visibility: visible');
-                            displayResults(response);
-                    }
-                },
-                error: function() {
-                    alert("Something went wrong.")
-                }
-            });
+var formSubmitHandler = function(event) {    
+    event.preventDefault();
+
+    var book = bookTitle.value.trim();
+
+    if (book) {
+        getBookTitles(book);
+
+        // clears old content
+        bookTitle.textContent = "";
+        bookAuth.textContent = "";
+        bookDescription = "";
+        bookPoster = "";
+    } else {
+        alert("Please enter a valid book title or author.");
+    }
+};
+
+// fetch function
+function fetchBookInfo() {
+    /* if(bookTitle.length === 0) {
+        bookTitle.textContent = "No titles found.";
+        return;
+    } */
+
+    fetch(apiUrlFirst + bookTitle)
+    .then(function (res) {
+        if(res.ok){
+            res.json().then(function(data){
+                console.log(data)
+                //renderPoster(data, book)
+                
+
+            })
         }
-        $("#search-bar").val(""); //clears search box
-    });
-    // function to display results in html
-    function displayResults(response) {
-        for(var i = 0; i < res.items.length; i+=2) {
-            item = res.items[i];
-            title1 = item.volumeInfo.title;
-            author1 = item.volumeInfo.author;
-            publisher1 = item.volumeInfo.publisher;
-            bookIsbn = item.volumeInfo.industryIdentifiers.identifier;
-            bookLink = item.volumeInfo.previewLink;
-            bookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
+    })
+    console.log(bookTitle)
+}
 
-            // output to output list
-            outputList.innerHTML += '<div class="row mt-4">' +
-            + formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn)
-            '</div>'
-        }
-    };
+function renderPoster(data){
+    console.log(data)
+    // bookPoster.attr("src", data.items.imageLinks.thumbnail)
+    bookTitle.text(data.volumeInfo.title)
+    bookDescription.text(data.volumeInfo.description)
+    bookAuth.text(data.volumeInfo.author)
 
-    //template for bulma cards
-    function formatOutput()
-    
-});
+}
+
+// event listener on cick
+document.getElementById("search-btn").addEventListener("click", fetchBookInfo())
